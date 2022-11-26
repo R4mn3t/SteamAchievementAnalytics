8fb363413d52425db0fe67d7d72b7e0d2db4accd
Erik Schnittker <dev@joyersch.de>
2022-11-26 23:35:27 +0100

    Updated args handling

--- SteamAchievementAnalytics/Program.cs
+++ SteamAchievementAnalytics/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using SteamAchievmentAnalytics;
 using System.IO;
 using System.Linq;
@@ -15,52 +16,102 @@ internal class Program
 
     private static async Task Main(string[] args)
     {
+        int cache = 0;
+        for (int i = 0; i < args.Length; i++)
+        {
+            cache = i;
+            if (args[i].StartsWith("-"))
+                switch (args[i].Substring(1).ToLower())
+                {
+                    case "h":
+                    case "-help":
+
+                        PrintHelp();
+
+                        break;
+
+                    case "la":
+                    case "-load-api":
+
+                        if (args.Length - i < 3)
+                        {
+                            Console.WriteLine("missing parameters after {0}", args[i]);
+                            return;
+                        }
+
+                        i += 2;
+                        try
+                        {
+                            bool hasNoGames = await GetFromApi(args[cache + 1], args[cache + 1]);
+                            if (hasNoGames)
+                                Console.WriteLine("This steam user might have no games!");
+                        }
+                        catch
+                        {
+                            Console.WriteLine(
+                                "Something went wrong accessing the api! Did you provide valid arguments?");
+                        }
+
+                        break;
+
+                    case "lf":
+                    case "-load-file":
+
+                        if (args.Length - i < 2)
+                            return;
+
+                        i += 1;
+                        
+                        if (!GetFromCache(args[cache + 1]))
+                            Console.WriteLine("Unable to load file");
+
+                        break;
+                    case "d":
+                    case "-dump":
+
+                        if (args.Length - i < 1)
+                        {
+                            Console.WriteLine("missing parameters after {0}", args[i]);
+                            return;
+                        }
+
+                        i += 1;
+
+                        DumpToFile(args[cache + 1]);
+
+                        break;
+                    case "p":
+                    case "-process":
+
+                        if (args.Length - i < 1)
+                        {
+                            Console.WriteLine("missing parameters after {0}", args[i]);
+                            return;
+                        }
+
+                        i += 1;
+
+                        Process(args[cache + 1]);
+                        break;
+                }
+        }
+
         // [params check]
         if (args.Length < 1)
             return;
         if (!args[0].StartsWith('-'))
             return;
-        switch (args[0].Substring(1).ToLower())
-        {
-            case "h":
-            case "-help":
-                Help();
-                break;
-            case "g":
-            case "-get":
-                if (args.Length != 3)
-                    return;
-                await Get(args[1], args[2]);
-                break;
-            case "c":
-            case "-cache":
-                if (args.Length != 2)
-                    return;
-                Cache(args[1]);
-                break;
-        }
-
         return;
     }
 
-    private static void Cache(string cache)
+    private static bool GetFromCache(string cacheFile)
     {
-        using StreamReader sr = new StreamReader(cache);
+        using StreamReader sr = new StreamReader(cacheFile);
         library = JsonConvert.DeserializeObject<Library>(sr.ReadToEnd()) ?? new Library();
-        if (library.Equals(new Library()))
-            return;
-        float? value = library.TotalCompletion();
-        using StreamWriter sw = new StreamWriter("games.txt");
-        var names = library.TotalStartedNamesSortedByCompletion(false);
-        foreach (string name in names)
-        {
-            sw.WriteLine(string.Format("{0}:{1}", name, library.CompletionByName(name)));
-        }
-
-        Console.WriteLine(value);
+        return !library.Equals(new Library());
     }
 
-    private static async Task Get(string key, string userId)
+    private static async Task<bool> GetFromApi(string key, string userId)
     {
         // save cursor position
         (int left, int top) cursor = Console.GetCursorPosition();
@@ -77,9 +128,9 @@ internal class Program
                        new Steam.API.userGames.UserGames();
 
         if (apiGames.Equals(new Steam.API.userGames.UserGames()))
-            return;
+            return false;
 
-        
+
         for (var index = 0; index < apiGames.response.games.Length; index++)
         {
             var apiGame = apiGames.response.games[index];
@@ -139,12 +190,45 @@ internal class Program
                 , apiGame.appid.ToString()));
         }
 
-        using StreamWriter sr = new StreamWriter(string.Format("{0}.json", userId));
-        sr.Write(JsonConvert.SerializeObject(library));
+        return true;
     }
 
-    private static void Help()
+    private static void Process(string type)
     {
-        Console.WriteLine("program -[option]\nprogram -g [key] [userId]\nprogram -c [cache]");
+        switch (type)
+        {
+            case "c":
+            case "completion":
+            case "comp":
+                Console.Write(library.TotalCompletion());
+                break;
+            default:
+                Console.WriteLine("Unable to find process {0}", type);
+                break;
+        }
+    }
+
+    private static void PrintHelp()
+    {
+        Console.Write(@"Args
+-----
+-h --help                               Displays this info
+-la --load-api      [apikey] [userid]   Loads user game data with the achievements from the api
+-lf --load-file     [file]              Loads user game data from a given file
+-d --dump           [file]              Dumps the user game data to a file (for -lf)
+-p --process        [process]           Outputs data based on a process
+
+Processes:
+c comp completion                       Prints out total completion average
+Examples:
+-la [Key] [userid] -d cache.json -p x1
+-lf cache.json -p x2
+");
+    }
+
+    private static void DumpToFile(string fileName)
+    {
+        using var streamWriter = new StreamWriter(string.Format(fileName));
+        streamWriter.Write(JsonConvert.SerializeObject(library));
     }
 }
